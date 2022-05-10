325703e43951637380abe97466e17aabbfed545b
demetriomagatti <demetriomagatti@gmail.com>
2022-05-11 00:19:04 +0200

    first real commit, actually

--- classes.r
+++ classes.r
@@ -3,15 +3,25 @@
 
 fields=list(name='character',
             job='character',
-            age='numeric',
-            dialogues='character')
-
+            age='numeric')
 
 methods=list(initialize=function(){
-                
+                name <<- 'Unknown'
+                age <<- 99
+                job <<- ''
                 },
                 
-            export_data=function(){
+             introduce=function(){
+                print(paste('Ciao! Mi chiamo ',
+                            name, 
+                            ' e ho ',
+                            age,
+                            ' anni!'))
+                },
+
+             celebrate_birthday=function(){
+                age <- age+1
+                print(age)
                 })
 
 setRefClass("givitino", fields=fields, methods=methods)
--- givitins.r
+++ givitins.r
@@ -1,17 +1,20 @@
+source('classes.r')
+
+
 #######################################################################################################################
 # derived class: Zanna
 
-fields = list(plant_list='character',
-              enthusiastic='logical')
+fields=list(plant_list='character',
+            enthusiastic='logical')
 
-methods = list(water_plants=function(){
-                    print('Ora vado ad innaffiare le piante!')
-                    if(enthusiastic){
-                        for(plant in plant_list){
-                            print(paste('Ho innaffiato la ', plant, '. Evviva!'))
-                            }
+methods=list(water_plants=function(){
+                print('Ora vado ad innaffiare le piante!')
+                if(enthusiastic){
+                    for(plant in plant_list){
+                        print(paste0('Ho innaffiato la ', plant, '. Evviva!'))
                         }
-                    })
+                    }
+                })
 
 setRefClass("Zanna",
             fields=fields,
@@ -22,25 +25,27 @@ setRefClass("Zanna",
 #######################################################################################################################
 # derived class: Mati
 
-fields = list(balls_of_wool='numeric',
-              tops='numeric',
-              savings='numeric',
-              enthusiastic='logical')
+fields=list(balls_of_wool='numeric',
+            tops='numeric',
+            savings='numeric',
+            enthusiastic='logical')
 
-methods = list(knit=function(){
-                    if(balls_of_wool>0){
-                        balls_of_wool <<- balls_of_wool-1
-                        tops <<- tops+1
-                    }else{
-                        print('Oh no! Come è possibile? Ho finito la lana!')
-                    }
-                },
-               refill=function(){
-                   if(savings>0){
-                       savings <<- savings-1
-                       balls_of_wool <<- balls_of_wool+1
-                   }
-               })
+methods=list(knit=function(){
+                if(balls_of_wool>0){
+                    balls_of_wool <<- balls_of_wool-1
+                    tops <<- tops+1
+                }else{
+                    print('Oh no! Come è possibile? Ho finito la lana!')
+                }
+             },
+             refill=function(){
+                if(savings>0){
+                    savings <<- savings-1
+                    balls_of_wool <<- balls_of_wool+1
+                }else{
+                    print('Oh no! Ho speso tutti i soldi in lana!')
+                }
+            })
 
 setRefClass("Mati",
             fields=fields,
